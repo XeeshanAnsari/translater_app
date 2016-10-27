d84765235e22a2399d0f78402d9cc5dab9470c86
xeeshan <xeeshan@gmail.com>
2016-10-27 00:38:57 -0700

    Complete to search without refresh page just write and result will be come

--- database/search.php
+++ database/search.php
@@ -1,28 +1,30 @@
 <?php
  
+ 
          mysql_connect("localhost","root","");
          mysql_select_db("database"); 
-            $search = $_POST['english'];
-          
-           $search =strtolower($search);
-           $search1 = explode(' ', $search);
-          
-          for($i=0; $i<count($search1); $i++){
-           
-              $query_search ="SELECT * from lang where english='$search1[$i]' ";
-              $run = mysql_query($query_search);
-   
         
-          while($row=mysql_fetch_array($run)){
-              $english =$row['english'];
-              $roman = $row['roman'];
-              $france = $row['france'];
-              
-              $result = $result . " " . $roman;
-              
-                        }
-          
-             }
-          echo "<script> window.open('../index.php?roman=$result ','_self')</script>" ;
+          $q = $_REQUEST["q"];
+          $result = "";
+          $q =strtolower($q);
+          $search = explode(' ', $q);
+           
+         for($i=0; $i<count($search); $i++){
+              $query_search ="SELECT * from lang where english= '$search[$i]' ";
+              $run = mysql_query($query_search);
+               
+               while($row=mysql_fetch_array($run)){
+                 $english =$row['english'];
+                 $roman = $row['roman'];
+                 $france = $row['france'];
+                 $result = $result . " " . $roman ;
+    
+                     }
+                 
+        
+         }  
+                 echo $result;
+             
+         
        
  ?>             
--- index.php
+++ index.php
@@ -3,12 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <title>index</title>
-    <link rel="stylesheet" href="./css/style.css">
+    
     <script src="./js/new.js"></script>
     
      <link rel="stylesheet" href="./css/bootstrap.min.css">
      <script src="./js/jquery.js"></script>
      <script src="./js/bootstrap.min.js"></script>
+     <link rel="stylesheet" href="./css/style.css">
+     <script>
+function showUser(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }
+      else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","./database/search.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+</script>
+
      
 </head>
 <body>
@@ -36,12 +62,16 @@
       <div>
           <h1>Translate</h1>
           <form action="./database/search.php" method="post">
-            <textarea name="english"   class="textarea" onkeyup="showHint(this.value)" ></textarea><br>
-            <input type="submit" value="search" name="search" class="btn btn-primary"  >
+            <textarea name="english"   class="textarea" onkeyup="showUser(this.value)" ></textarea><br>
+            
           </form>
  
        <br>
-       <textarea name="roman" id="txtHint" class="textarea" ><?php echo @$_GET['roman']; ?></textarea><br>       
+       <div class="well "  >
+            
+           <p > <span id="txtHint" ></span></p>
+       </div>
+              
   </div>
     
    
